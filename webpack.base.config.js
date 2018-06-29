e1721130a0b567a2ad517a17add05dc73cbca1d0
yeyan1996 <you@example.com>
2018-06-29 20:54:48 +0800

    dist提交

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         filename: '[name].[hash].js',
         path: path.join(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: './'
     },
     module: {
         rules: [
